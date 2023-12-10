154757654bb2f6dd3625b52d94dc777092c738ab
vlasiszacharis <vlasisz983@gmail.com>
2023-12-10 10:43:31 +0200

    responsive button-close added

--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -6,11 +6,21 @@ import "./NavBar.css"
 import logo from "../../assets/logo-modified.png"
 import { useState } from "react"
 import Popup from "../../components/Popup/Popup"
+import { IoMenu } from "react-icons/io5"
+import { IoClose } from "react-icons/io5"
+
 function NavBar() {
+  //  useState --> popum menu
   const [toggle, setToggle] = useState(false)
+  //  useState --> responsive menu
+  const [toggleMenu, setToggleMenu] = useState(true)
 
   const handleClick = () => {
     setToggle(!toggle)
+  }
+
+  const handleClickMenu = () => {
+    setToggleMenu(!toggleMenu)
   }
   return (
     <>
@@ -28,8 +38,12 @@ function NavBar() {
           <Link to='help'>Help</Link>
         </div>
         <div className='main_navbar-links_download'>
+          {/* click download app -->popum menu */}
           <button onClick={handleClick}>Download App</button>
           {toggle === true ? <Popup /> : null}
+        </div>
+        <div className='main__navbar-links_menu'>
+          {toggleMenu === true ? <IoMenu onClick={handleClickMenu} /> : <IoClose onClick={handleClickMenu} />}
         </div>
       </div>
     </>
