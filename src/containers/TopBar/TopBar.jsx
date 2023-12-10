7f48839f85ef848e60922f76c0f0e37f1cba0782
vlasiszacharis <vlasisz983@gmail.com>
2023-12-10 09:27:52 +0200

    responsive vol2

--- src/containers/TopBar/TopBar.jsx
+++ src/containers/TopBar/TopBar.jsx
@@ -5,7 +5,7 @@ import "./TopBar.css"
 function TopBar() {
   return (
     <>
-      <div className='Main__Topbar'>
+      <div className='main__topbar'>
         <div className='main__topbar-links'>
           <Link to='partner'>Partner</Link>
           <Link to='socials'>Socials</Link>
