e5c08ad29bfc42c1cf98c9ed6b4dd532e1bfc3b7
vlasiszacharis <vlasisz983@gmail.com>
2023-12-11 14:10:14 +0200

    popup responsive

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,19 +1,34 @@
 import React from "react"
 import "../Popup/Popup.css"
-
+import logo_ios from "../../assets/ios.svg"
+import logo_android from "../../assets/android.svg"
 function Popup() {
   return (
     <>
       <div className='popup__window'>
         <div className='popup__window-up'>
-          <h2>Download the App</h2>
-          <h3>Charger your EV</h3>
+          <span className='popup__window-up_title'>
+            <h2>Download the EVDC App!</h2>
+          </span>
+          <span className='popup__window-up_text'>
+            <h3>Charge your EV</h3>
+          </span>
+
           <div className='popup__window-up_app'>
-            <div className='popup__window-up_android'> android </div>
-            <div className='popup__window-up_ios'> ios </div>
+            <div className='popup__window-up_android'>
+              {" "}
+              <img src={logo_android} alt='android' />{" "}
+            </div>
+            <div className='popup__window-up_ios'>
+              {" "}
+              <img src={logo_ios} alt='ios' />{" "}
+            </div>
           </div>
         </div>
-        <div className='popup__window-down'>right</div>
+        <div className='popup__window-down'>
+          <span>barcode</span>
+          <span>text</span>
+        </div>
       </div>
     </>
   )
